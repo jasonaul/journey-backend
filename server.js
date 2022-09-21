1d1cca94c396804cdfdc674295fd370f785bddfd
jasonaul <jason.aul@gmail.com>
2022-09-21 19:27:27 -0400

    External modules, middleware, and models added. Schema added. Home route added.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* == External Modules == */
 const express = require('express')
-
+const methodOverride = require('method-override');
+require("dotenv").config()
 
 /* == Express Instance == */
 const app = express()
@@ -9,10 +10,23 @@ const app = express()
 /* == Middleware == */
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'));
 
 /* == Port == */
 const PORT = /* process.env.PORT || */ 3000;
+
+/* == Models Required == */
+const Events = require("./models/events.js");
+
+//*****************//
+//**** Routes ****//
+//*****************//
+
+/* == Home == */
+app.get('/', (req, res) => {
+    res.send("You are home.")
+})
 
 
 
