9d4dab9c6425421e47793937b3d9e31ddcb6e369
mickey-40 <mickeyarnold53@yahoo.com>
2022-09-27 21:20:35 -0500

    fix error

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ const corsOptions = {
 }
 
 /* == Middleware == */
-app.use(cors(corsOptions))
-// app.use(cors())
+// app.use(cors(corsOptions))
+app.use(cors('*'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // app.use(methodOverride('_method'))
