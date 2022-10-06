bef8a2e3f9a6b5ed5e1078a32fd5428c7c364974
mickey-40 <mickeyarnold53@yahoo.com>
2022-10-05 22:28:53 -0500

    changed user schema

--- models/users.js
+++ models/users.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    username: {type: String, required: true, unique: true},
+    name: {type: String, required: true, unique: true},
     password: String
 })
 
