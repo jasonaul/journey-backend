16c4a3e76391d3d041146995874e51e426799567
mickey-40 <mickeyarnold53@yahoo.com>
2022-10-05 22:34:46 -0500

    fixed user model

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
     password: String
 })
 
